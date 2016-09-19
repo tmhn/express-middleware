d4d484909d7947e9449b607d0052a0aecbdb4c94
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-09-19 23:46:54 +0100

    add post function to create books and form on index.html page

--- server.js
+++ server.js
@@ -3,10 +3,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.json())
+var path = require('path');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 var port = 3030 || process.env.PORT;
-
+//app.use(express.json());       // to support JSON-encoded bodies
+//app.use(express.urlencoded()); // to support URL-encoded bodies
+//app.use(express.json() );
+//app.use(express.urlencoded() );
+//app.use( json() );
+//app.use( urlencode() );
+//app.use(bodyParser.json());
 
 
 var books = {
@@ -23,12 +31,17 @@ var books = {
 		"author": "Arthur Conan Doyle",
 		"id": 456
 	}]
-}
+};
 
 // Books
 app.get('/books', function(req, res) {
+	console.log()
 	res.send(books);
-})
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.get('/books/:id', function(req, res) {
 	var paramsId = req.params.id;
@@ -48,6 +61,17 @@ app.get('/books/:id', function(req, res) {
 	res.send(result);
 })
 
-app.listen(port, function() {
+app.post('/books', function(req, res){
+	var newBook = {
+		id : req.body.id,
+		name : req.body.name,
+		author : req.body.author
+		}
+	res.send(newBook);
+	books.library.push(newBook);
+	books.save();
+});
+
+app.listen(port, function(req, res) {
 	console.log("Listening on: " + port);
 })
